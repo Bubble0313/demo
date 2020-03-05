1be05ef6eb6328794cb1d00d990bc03a66517771
9850188 <a9850188@9850188s-MacBook-Pro.local>
2020-03-05 09:58:59 +0000

    first commit

---
+++ src/main/java/com/example/demo/model/HolidayInfo.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HolidayInfo {
+    String personName;
+    String holidayTime;
+    String holidayLocation;
+}
